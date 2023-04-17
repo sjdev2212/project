fbf9f52163133fd1b9f2214604abf55a36e15613
Anibal Amoroso <76854002+sj1978@users.noreply.github.com>
2023-04-17 14:40:09 -0300

    hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Hello from users");
+    res.send("Hello from hotels");
     });
 
 export default router;
